99e4a3141c5a06533b1ef49a7f0f6b1c25446493
Jerome Arzel <jerome@quitoque.fr>
2019-01-03 10:58:53 +0100

    Initial commit with base code

---
+++ README.md
@@ -0,0 +1,54 @@
+# hdlbr - Handlebars CLI
+
+## Usage
+
+````sh
+hdlbr VARIABLES_FILE TEMPLATE_FILE
+```
+
+Where:
+- VARIABLES_FILE is a JSON map, listing all vars.
+- TEMPLATE_TILE is a Handlebars template.
+
+## Example
+
+`vars.json`:
+
+```json
+{
+  "name": "Foo",
+  "comments": [
+    {
+      "author": "Dracula"
+    },
+    {
+      "author": "Alucard"
+    }
+  ]
+}
+```
+
+`template.hbs`:
+
+```handlebars
+Hello {{name}}
+{{~#each comments}}
+  {{author}}
+{{~/each~}}
+```
+
+Result:
+
+```
+Hello Foo
+  Dracula
+  Alucard
+```
+
+## Build
+
+You may use Rust 2018 and Cargo to build:
+
+```
+cargo build --release
+```
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use handlebars::Handlebars;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+use std::path::Path;
+use std::error::Error;
+use serde_json::Map;
+use serde_json::value::Value;
+use clap::clap_app;
+
+fn read_vars_from_file<P: AsRef<Path>>(path: P) -> Result<Map<String, Value>, Box<Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let vars = serde_json::from_reader(reader)?;
+
+    Ok(vars)
+}
+
+fn main() -> Result<(), Box<Error>> {
+    let matches = clap_app!(myapp =>
+        (version: "1.0")
+        (author: "Jérôme Arzel <jerome@quitoque.fr>")
+        (about: "Tera CLI")
+        (@arg VARIABLES_FILE: +required "Variables file (JSON format).")
+        (@arg TEMPLATE_FILE: +required "Handlebars template file")
+    ).get_matches();
+
+    let vars_file_path = matches.value_of("VARIABLES_FILE").unwrap();
+    let template_path = matches.value_of("TEMPLATE_FILE").unwrap();
+
+    let vars = read_vars_from_file(vars_file_path)?;
+    let mut tpl_str = String::new();
+    let mut file = File::open(template_path)?;
+
+    file.read_to_string(&mut tpl_str)?;
+
+    let reg = Handlebars::new();
+
+    println!("{}", reg.render_template(&tpl_str, &vars)?);
+
+    Ok(())
+}
